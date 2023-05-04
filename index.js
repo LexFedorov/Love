6f51bacbb17629a6bab236c4407e43b441c0cb31
AlexFedorov <132588654+LexFedorov@users.noreply.github.com>
2023-05-04 17:45:49 +0300

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function(){
+    output.innerHTML = 'Я ТЕБЯ ЛЮБЛЮ';
+}
+
+);
